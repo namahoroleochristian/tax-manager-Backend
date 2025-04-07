5a237e4e74cfed68d707791176df26441f4dc0cc
namahoroleochristian <leonamahoro1@gmail.com>
2025-04-07 08:46:55 -0700

    login and support model created for database

--- backend/routes/routes.route.js
+++ backend/routes/routes.route.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { Customer_Register, TaxPayer_register } from '../controllers/user.controller.js'
+import { Customer_Register, Support_Register, TaxPayer_register } from '../controllers/user.controller.js'
 const app = express()
 export const Router = express.Router()
 
 
 Router.post('/payer/register',TaxPayer_register)
 Router.post('/customer/register',Customer_Register)
+Router.post('/support/register',Support_Register)
