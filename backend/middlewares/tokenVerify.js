79d24a474ca8b2ff86dc84926b919654316d7039
namahoroleochristian <leonamahoro1@gmail.com>
2025-04-30 11:53:14 -0700

    token cleans

---
+++ backend/middlewares/tokenVerify.js
@@ -0,0 +1,23 @@
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+dotenv.config()
+
+
+export const tokenVerify  = async (req,res,next) => { 
+    const token = req.body || req.cookies.jwt;
+    if(!token){
+        return res.status(404).json({success:false, message:'token not found'})
+    }
+    try {
+        await jwt.verify(token,process.env.SECRET_KEY,(err,decoded) => {
+            if(err){
+                res.status(403).json({success: false,message:"token is invalid hence unauthorised"})
+            }
+            req.user = decoded
+            next()
+        })
+    } catch (error) {
+        res.status(500).json({success: false,message:error.message})
+        
+    }
+}
